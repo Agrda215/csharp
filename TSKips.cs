bcbc6e474940f2507fb2c61df9d736e67f0b22c3
Agrda215 <107811732+Agrda215@users.noreply.github.com>
2022-08-21 10:31:12 -0300

    Create TSKips.cs

---
+++ TSKips.cs
@@ -0,0 +1,160 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Typescript Kips!");
+        Console.WriteLine("Based of C# Kips of Typescript Lang!");
+        Console.WriteLine("Level 1");
+        Console.WriteLine("var c = 0");
+        
+        // Get Input
+        // Level 1  Input
+        string check1 = Console.ReadLine();
+        
+        if (check1 == "3")
+        {
+            Console.WriteLine("Completed Level 1!");
+            Console.WriteLine("Level 2");
+            Console.WriteLine("Completed Out: var c = 3");
+            Console.WriteLine("C# | LV2");
+            Console.WriteLine("class MyGoaled {");
+            Console.WriteLine("  constructor() {");
+            Console.WriteLine("    this.j = 1");
+            Console.WriteLine("   }");
+            Console.WriteLine("}");
+        }
+        
+        // Level 2 Input
+        string check2 = Console.ReadLine();
+        
+        if (check2 == "11")
+        {
+            Console.WriteLine("Completed Level 2!");
+            Console.WriteLine("Level 3");
+            Console.WriteLine("Completed Out: class...");
+            Console.WriteLine("C# | LV3");
+            Console.WriteLine("const b = 2.2");
+        }
+        
+        // Level 3 Input
+        string check3 = Console.ReadLine();
+        
+        if (check3 == "17")
+        {
+            Console.WriteLine("Completed Level 3!");
+            Console.WriteLine("Level 4");
+            Console.WriteLine("Completed Out: const b = 17");
+            Console.WriteLine("C# | LV4");
+            Console.WriteLine("const dok = 2.4");
+        }
+        
+        // Level 4 Input
+        string check4 = Console.ReadLine();
+        
+        if (check4 == "15")
+        {
+            Console.WriteLine("Completed Level 4!");
+            Console.WriteLine("Level 5");
+            Console.WriteLine("Completed Out: const dok = 15");
+            Console.WriteLine("C# | LV5");
+            Console.WriteLine("const hello = 2.56");
+        }
+        
+        // Level 5 Input
+        string check5 = Console.ReadLine();
+        
+        if (check5 == "13")
+        {
+            Console.WriteLine("Completed Level 5!");
+            Console.WriteLine("Level 6");
+            Console.WriteLine("Completed Out: const hello = 13");
+            Console.WriteLine("C# | LV6");
+            Console.WriteLine("const hello1 = 2.76");
+        }
+        
+        // Level 6 Input
+        string check6 = Console.ReadLine();
+        
+        if (check6 == "14")
+        {
+            Console.WriteLine("Completed Level 6!");
+            Console.WriteLine("Level 7");
+            Console.WriteLine("Completed Out: const hello1 = 14");
+            Console.WriteLine("C# | LV7");
+            Console.WriteLine("const heno = 2.96");
+        }
+        
+        // Level 7 Input
+        string check7 = Console.ReadLine();
+        
+        if (check7 == "12")
+        {
+            Console.WriteLine("Completed Level 7!");
+            Console.WriteLine("Level 8");
+            Console.WriteLine("Completed Out: const heno = 12");
+            Console.WriteLine("C# | LV8");
+            Console.WriteLine("const heno1 = 2.96");
+        }
+        
+        // Level 8 Input
+        string check8 = Console.ReadLine();
+        
+        if (check8 == "18")
+        {
+            Console.WriteLine("Completed Level 8!");
+            Console.WriteLine("Level 9");
+            Console.WriteLine("Completed Out: const heno1 = 18");
+            Console.WriteLine("C# | LV9");
+            Console.WriteLine("const heno2 = 3.12");
+        }
+        
+        // Level 9 Input
+        string check9 = Console.ReadLine();
+        
+        if (check9 == "16")
+        {
+            Console.WriteLine("Completed Level 9!");
+            Console.WriteLine("Level 10");
+            Console.WriteLine("Completed Out: const heno2 = 16");
+            Console.WriteLine("C# | LV10");
+            Console.WriteLine("const henos = 3.32");
+        }
+        
+        // Level 10 Input
+        string check10 = Console.ReadLine();
+        
+        if (check10 == "12")
+        {
+            Console.WriteLine("Completed Level 10!");
+            Console.WriteLine("Level 11");
+            Console.WriteLine("Completed Out: const henos = 12");
+            Console.WriteLine("C# | LV11");
+            Console.WriteLine("const henos1 = 3.48");
+        }
+        
+        // Level 11 Input Last One!
+        string check11 = Console.ReadLine();
+        
+        if (check11 == "12")
+        {
+            Console.WriteLine("Completed Level 11!");
+            Console.WriteLine("Level 12");
+            Console.WriteLine("Completed Out: const henos1 = 13");
+            Console.WriteLine("C# | LV12");
+            Console.WriteLine("const henoswin = 3.48");
+        }
+        
+        // Level 12 Win
+        string check12 = Console.ReadLine();
+        
+        if (check12 == "15")
+        {
+            Console.WriteLine("You Win!");
+        }
+    }
+}
